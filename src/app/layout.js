22646bfcf39011d8beef6ff6293a135928f0cf54
Kirill Ginko <53977030+kirillginko@users.noreply.github.com>
2025-02-17 20:16:30 -0500

    disable scale on mobile

--- src/app/layout.js
+++ src/app/layout.js
@@ -83,6 +83,12 @@ export default function RootLayout({ children }) {
   return (
     <GlobalStateProvider>
       <html lang="en" suppressHydrationWarning>
+        <head>
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"
+          />
+        </head>
         <body
           className={`${instructionFont.variable} ${AustinCyRoman.variable}`}
           suppressHydrationWarning
